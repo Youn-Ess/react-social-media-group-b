1ccf9e9fd16374a4cec96aa3dbb6a17fbb378ead
youness <younessaithadou9@gmail.com>
2024-02-03 15:54:28 +0100

    add material-tailwind library

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,40 @@
 import { useContext } from "react";
 import { MyContext } from "../../utils/contextProvider";
 
+import { Input } from "@material-tailwind/react";
+
+
+
 export const LoginPage = () => {
     const [dataBase, setDataBase] = useContext(MyContext)
     console.log(dataBase);
 
+
+
+
+
     return (
         <>
-            <h1>login page</h1>
+            {/* <div className="bg-[#2a76e7] ">
+                <div className="w-[80%] ">
+                    <div className="">
+                        <h1>LOGO</h1>
+                        <p>description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, voluptatem.</p>
+                    </div>
+
+                    <form action="">
+                        <label htmlFor="">Email</label>
+                        
+                    </form>
+                </div>
+            </div> */}
+            <div className="w-72">
+                <Input label="Username" />
+            </div>
+
+
+
+
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 
+const withMT = require("@material-tailwind/react/utils/withMT");
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withMT({
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     'node_modules/flowbite-react/lib/esm/**/*.js',
@@ -11,4 +12,4 @@ module.exports = {
   plugins: [
     require('flowbite/plugin'),
   ],
-}
+});
