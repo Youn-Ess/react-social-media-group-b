c865ec33df8b7bfc72108aed3cb08bd71d6e28af
youness <younessaithadou9@gmail.com>
2024-02-06 01:12:24 +0100

    add forget password authentication

--- src/pages/Login/components/SignUpSection.jsx
+++ src/pages/Login/components/SignUpSection.jsx
@@ -31,134 +31,139 @@ export const SignUpSection = ({ step, setStep }) => {
         e.preventDefault()
         let newTab = [...dataBase]
 
-        let userExist = newTab.findIndex(element => element.userName == userName)
+        let userNameExist = newTab.findIndex(element => element.userName == userName)
+        let emailExist = newTab.findIndex(element => element.email == email)
 
         if (firstName && lastName && userName && email && phoneNumber && password && confirmPassword && day && month && year && gender) {
-            if (userExist == -1) {
-                // ^ first name , last name and username
-                if (Char.specialChars.test(firstName && lastName && userName)) {
-                    return alert(`you can't use those special characters (${Char.specialChars}) on your first , last name or username`)
-                }
-                if (Char.numbers.test(firstName && lastName && userName)) {
-                    return alert(`you can't use numbers on your first, last name or username`);
-                }
-                if (firstName.length < 5 && lastName.length < 5 && userName) {
-                    return alert(`you name is to short please enter a name that have more than 5 charracters`);
-                }
+            if (userNameExist == -1) {
+                if (emailExist == -1) {
+                    // ^ first name , last name and username
+                    if (Char.specialChars.test(firstName && lastName && userName)) {
+                        return alert(`you can't use those special characters (${Char.specialChars}) on your first , last name or username`)
+                    }
+                    if (Char.numbers.test(firstName && lastName && userName)) {
+                        return alert(`you can't use numbers on your first, last name or username`);
+                    }
+                    if (firstName.length < 5 && lastName.length < 5 && userName) {
+                        return alert(`you name is to short please enter a name that have more than 5 charracters`);
+                    }
 
-                // ^ email
+                    // ^ email
 
-                // Check for leading or trailing spaces.
-                // Convert all letters to lowercase.
-                setEmail(email.trim().toLowerCase())
-                // Do not save the Email if it has spaces in the middle.
-                if (email.includes(` `)) {
-                    alert(`your email can't contain spaces`);
-                    return
-                }
+                    // Check for leading or trailing spaces.
+                    // Convert all letters to lowercase.
+                    setEmail(email.trim().toLowerCase())
+                    // Do not save the Email if it has spaces in the middle.
+                    if (email.includes(` `)) {
+                        alert(`your email can't contain spaces`);
+                        return
+                    }
 
-                // Do not save the Email if it contain more that one "@" symbol.
-                if (email.split(``).filter(char => char == `@`).length > 1) {
-                    return alert(`you can't put two atmarks at your email`);
-                }
-                // Do not save the Email if it does not contain exactly one "@" symbol.
-                if (!email.includes(`@`)) {
-                    alert(`your email munst contain @ character`);
-                    return
-                }
-                // Do not save the Email if it has fewer than 10 characters (excluding spaces).
-                if (email.split(`@`)[1].length == 0) {
-                    alert(`your email munst end with some thing like gmail.com`);
-                    return
-                }
-                // Do not save the Email if it does not contain exactly one "." symbol.
-                if (!email.split(`@`)[1].includes(`.`)) {
-                    alert(`your email munst containe a . after the atmark`);
-                    return
-                }
-                if (email.split(`@`)[0].length < 5) {
-                    alert(`your email munst contain 5 characters or more before atmark`);
-                    return
-                }
-
-
-                // ^ phone number
-
-                if (!Char.numbers.test(phoneNumber)) {
-                    return alert(`enter numbers on phone number input`);
-                }
-
-                // ^ check password
-
-                // Check for leading or trailing spaces.
-                setPassword(password.trim())
-                // Do not save the Password if it has spaces in the middle.
-                if (password.includes(` `)) {
-                    alert(`your password can't contain spaces`)
-                    return
-                }
-                // Require at least one special character from the set: ["@", "#", "-", "+", "*", "/"].
-                if (!Char.specialChars.test(password)) {
-                    alert(`you must put some special characters on your password`)
-                    return
-                }
-                // Require at least 7 characters to confirm the password.
-                if (password.length < 7) {
-                    alert(`you password is to short please enter a password that have more than 7 charracters`)
-                    return
-                }
-
-                if (password != confirmPassword) {
-                    return alert(`password not match`)
-                }
-
-                // ^ birth day
-                if (!Char.numbers.test(day) && !Char.numbers.test(month) && year && !Char.numbers.test(year)) {
-                    return alert(`enter numbers on birthday inputs`)
-                }
-
-                if (day > 31 || day == 0) {
-                    return alert(`day not valide`)
-                }
-                if (month > 12 || month == 0) {
-                    return alert(`month not valide`)
-                }
-                if (year > 2024 || year < 1950 || year == 0) {
-                    return alert(`year not valide`)
-                }
+                    // Do not save the Email if it contain more that one "@" symbol.
+                    if (email.split(``).filter(char => char == `@`).length > 1) {
+                        return alert(`you can't put two atmarks at your email`);
+                    }
+                    // Do not save the Email if it does not contain exactly one "@" symbol.
+                    if (!email.includes(`@`)) {
+                        alert(`your email munst contain @ character`);
+                        return
+                    }
+                    // Do not save the Email if it has fewer than 10 characters (excluding spaces).
+                    if (email.split(`@`)[1].length == 0) {
+                        alert(`your email munst end with some thing like gmail.com`);
+                        return
+                    }
+                    // Do not save the Email if it does not contain exactly one "." symbol.
+                    if (!email.split(`@`)[1].includes(`.`)) {
+                        alert(`your email munst containe a . after the atmark`);
+                        return
+                    }
+                    if (email.split(`@`)[0].length < 5) {
+                        alert(`your email munst contain 5 characters or more before atmark`);
+                        return
+                    }
 
 
-                const user = {
-                    firstName: firstName,
-                    lastName: lastName,
-                    userName: userName,
-                    email: email,
-                    phoneNumber: phoneNumber,
-                    password: password,
-                    gender: gender,
-                    birthDay: birthDay,
-                    bio: ``,
-                    infos:
-                    {
-                        jobe: ``,
-                        schooling: ``,
-                        city: ``,
-                        relationship: ``
-                    },
-                    followers: [],
-                    following: [],
-                    myGroups: [],
-                    profileImage: null,
-                    coverProfileImage: null
+                    // ^ phone number
+
+                    if (!Char.numbers.test(phoneNumber)) {
+                        return alert(`enter numbers on phone number input`);
+                    }
+
+                    // ^ check password
+
+                    // Check for leading or trailing spaces.
+                    setPassword(password.trim())
+                    // Do not save the Password if it has spaces in the middle.
+                    if (password.includes(` `)) {
+                        alert(`your password can't contain spaces`)
+                        return
+                    }
+                    // Require at least one special character from the set: ["@", "#", "-", "+", "*", "/"].
+                    if (!Char.specialChars.test(password)) {
+                        alert(`you must put some special characters on your password`)
+                        return
+                    }
+                    // Require at least 7 characters to confirm the password.
+                    if (password.length < 7) {
+                        alert(`you password is to short please enter a password that have more than 7 charracters`)
+                        return
+                    }
+
+                    if (password != confirmPassword) {
+                        return alert(`password not match`)
+                    }
+
+                    // ^ birth day
+                    if (!Char.numbers.test(day) && !Char.numbers.test(month) && year && !Char.numbers.test(year)) {
+                        return alert(`enter numbers on birthday inputs`)
+                    }
+
+                    if (day > 31 || day == 0) {
+                        return alert(`day not valide`)
+                    }
+                    if (month > 12 || month == 0) {
+                        return alert(`month not valide`)
+                    }
+                    if (year > 2024 || year < 1950 || year == 0) {
+                        return alert(`year not valide`)
+                    }
+
+
+                    const user = {
+                        firstName: firstName,
+                        lastName: lastName,
+                        userName: userName,
+                        email: email,
+                        phoneNumber: phoneNumber,
+                        password: password,
+                        gender: gender,
+                        birthDay: birthDay,
+                        bio: ``,
+                        infos:
+                        {
+                            jobe: ``,
+                            schooling: ``,
+                            city: ``,
+                            relationship: ``
+                        },
+                        followers: [],
+                        following: [],
+                        myGroups: [],
+                        profileImage: null,
+                        coverProfileImage: null
+                    }
+
+                    newTab.push(user)
+                    setDataBase(newTab)
+
+                    setConnected([...connected, user])
+                } else {
+                    return alert(`email existe`)
                 }
-
-                newTab.push(user)
-                setDataBase(newTab)
-
-                setConnected([...connected, user])
 
             } else {
-                return alert(`user exist`)
+                return alert(`username existe`)
             }
 
         } else {
